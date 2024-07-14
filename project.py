03b39f90041bbe88b8125c22fcefa8133aa125a2
Norbert <dsw52680@student.dsw.edu.pl>
2024-07-14 21:31:59 +0200

    Create project.py

---
+++ project.py
@@ -0,0 +1,12 @@
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Data conversion program.')
+    parser.add_argument('input_file', type=str, help='Path to the input file.')
+    parser.add_argument('output_file', type=str, help='Path to the output file.')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    print(f'Input file: {args.input_file}')
+    print(f'Output file: {args.output_file}')
